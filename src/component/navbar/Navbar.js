7279da3fd8e158c3e09dbe50587756dd27f8538a
bsihame <bsihame@hotmail.com>
2021-08-28 06:21:22 -0400

    remove google map code

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,24 +1,25 @@
 import React from "react";
 // import { Router, Link } from "react-router-dom";
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
-export default function Navbar () {
-  
-    return (
-    <>
-      <NavLink to='/' activeStyle>
+export default function Navbar() {
+  return (
+    <div>
+      <NavLink to="/" activeStyle>
         Home
       </NavLink>
-      <NavLink to='/about' activeStyle>
+
+      <NavLink to="/about" activeStyle>
         About
       </NavLink>
-      <NavLink to='/album' activeStyle>
+
+      <NavLink to="/album" activeStyle>
         Album
       </NavLink>
-      <NavLink to='/contact' activeStyle>
+
+      <NavLink to="/contact" activeStyle>
         Contact
       </NavLink>
-    </>
-    )
-  
+    </div>
+  );
 }
